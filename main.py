25d142cd9c19305e4d3c76610e790477e0560ac7
Ayres Nogueras <120499206+AyresNogueras@users.noreply.github.com>
2023-10-06 14:29:47 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+#Opening greeting using print
+print("Welcome to the best band name generator ever!")
+#City is now a variable for the city the user grew up in using input
+city = input("What city did you grow up in?\n")
+#pet_name is now a varable for the user's pet using input
+pet_name = input ("What is the name of your pet?\n")
+#Using the variables from the user's entered information, you then get a generated band name
+print("Your band name could be " + city + " " + pet_name)
